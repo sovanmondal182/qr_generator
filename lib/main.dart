6195c9cacd9c1c95f25081a22f79e2a300464a63
Sovan Mondal <sovanmondal182@gmail.com>
2023-01-09 20:59:34 +0530

    debug banner removed

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
             create: (context) => QrGeneratorProvider()),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
